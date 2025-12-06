3a07ff1aac837450efc3467c5edb900a6885f668
abdul-kasif <abdulkasif.ra@gmail.com>
2025-12-06 23:38:13 +0530

    learnt malloc and dynamic allocation

---
+++ C/max_num_array/max_num_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n;
+  if (scanf("%d", &n) != 1 || n <= 0) {
+    fprintf(stderr, "Invalid input\n");
+    return 1;
+  }
+
+  // Allocation to the heap
+  int *arr = malloc(n * sizeof(*arr));
+  if (arr == NULL) {
+    fprintf(stderr, "Allocation Failed\n");
+    return 1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (scanf("%d", &arr[i]) != 1) {
+      fprintf(stderr, "Invalid array element\n");
+      free(arr);
+      return 1;
+    }
+  }
+
+  printf("%d\n", arr[0]);
+  free(arr);
+  return 0;
+}
