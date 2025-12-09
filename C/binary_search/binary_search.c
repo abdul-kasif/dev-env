2b562f305ef540a9e1d7556b901c361f97e5caf7
abdul-kasif <abdulkasif.ra@gmail.com>
2025-12-09 16:03:16 +0530

    add: implement binary search and find the first occurance of tbe target

--- C/binary_search/binary_search.c
+++ C/binary_search/binary_search.c
@@ -4,17 +4,19 @@
 int binarySearch(const int *arr, int n, int target) {
   int low = 0;
   int high = n - 1;
+  int index = -1;
   while (low <= high) {
     int mid = low + (high - low) / 2;
     if (arr[mid] == target) {
-      return mid;
+      index = mid;
+      high = mid - 1;
     } else if (arr[mid] < target) {
       low = mid + 1;
     } else if (arr[mid] > target) {
       high = mid - 1;
     }
   }
-  return -1;
+  return index;
 }
 int main(void) {
   int n, target;
