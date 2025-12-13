87425c7407b4658b79eb52f215afc7283914daa7
abdul-kasif <abdulkasif.ra@gmail.com>
2025-12-13 18:09:08 +0530

    add: find the peak element in O(log n)

---
+++ C/peak_element_binary/peak_element_binary.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+int peakElement(const int *arr, int n) {
+  int low = 0;
+  int high = n - 1;
+
+  while (low < high) {
+    int mid = low + (high - low) / 2;
+    if (arr[mid] < arr[mid + 1]) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+  return low;
+}
+int main(void) {
+  int n;
+  if (scanf("%d", &n) != 1 || n <= 0) {
+    fprintf(stderr, "Invalid Input\n");
+    return 1;
+  }
+
+  int *arr = malloc(n * sizeof(*arr));
+  if (arr == NULL) {
+    fprintf(stderr, "Allocation Failed\n");
+    return 1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (scanf("%d", &arr[i]) != 1) {
+      fprintf(stderr, "Invalid Input\n");
+      free(arr);
+      return 1;
+    }
+  }
+  int res = peakElement(arr, n);
+  printf("%d\n", res);
+
+  free(arr);
+  return 0;
+}
